3f91fd7185477c00ba9730e8afde59cdd5310ccb
root <nsea@163.com>
2025-05-30 17:08:35 +0800

    测试数据

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import '@/style/index.scss'
 import '@/day_06/style/index.scss'
 import { Provider } from 'react-redux';
 import store from '@/day_06/store'
-import App from '@/day_08/App'
+import App from '@/day_01/App'
 
 
 // 渲染根组件到#root
